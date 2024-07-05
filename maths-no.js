0bb08829dc3df341129763a4bb98f111d1f694f4
Vish-Sharma07 <120493508+Vish-Sharma07@users.noreply.github.com>
2024-07-05 22:06:09 +0530

    Add files via upload

---
+++ maths-no.js
@@ -0,0 +1,13 @@
+const score = 400
+console.log(score); // undefined
+
+const balance = new Number(1234);
+console.log(balance); // Specifically specifies that it is a Number
+
+console.log(balance.toString());
+console.log(typeof(balance));
+
+console.log(balance.toFixed(1)); // gives a fixed value after decimal i.e. 1 in this case
+
+const otherNumber = 100000
+console.log(Math.round(4.6));
---
+++ strings.js
@@ -0,0 +1,8 @@
+const name = "Vishakha";
+const repoCount = 12;
+
+// console.log(name + repoCount + "value");  Old Syntax
+
+// Modern Syntax
+
+console.log(`Hello, my name is ${name} and my Repo Count is ${repoCount}`);
