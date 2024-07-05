ed51fba67e1bb2a3a5282f5bc2ce12ff6cced85f
Vish-Sharma07 <120493508+Vish-Sharma07@users.noreply.github.com>
2024-07-05 06:30:09 +0530

    Add files via upload

---
+++ dataTypes.js
@@ -0,0 +1,20 @@
+"use strict" //Treat all JS code as newer version.
+
+// alert(4 + 5) // we are using node.js not browseer
+// number = 2 to power 53
+// BigInt
+// boolean => true or false
+// string = ""
+// undefined
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
+
+
+/*Primitive uses Stack Memory
+There are 7 data type = string, Number, Boolean, BognInt, Null, Symbol, Undefined
+
+Non= Primitive (Reference) = Uses Heap Memory
+Arrays Objects, Functions
+*/
---
+++ variables.js
@@ -0,0 +1,6 @@
+const accountId = 12345;
+let accountEmail = "vish@gmail.com";
+var accountPassword = "124545"; // var is not preferred just because of block and functional scope
+accountCity = "Meerut";
+let accountState;
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
